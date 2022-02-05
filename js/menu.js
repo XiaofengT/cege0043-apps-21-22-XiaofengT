68b274c2c44d4399e37854d143b7429f50c54a8d
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-02-05 16:02:45 +0000

    Adding pop up alert for menu 3,4,7,9,10

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,21 @@
 function menu1(){
 	alert("menu number is 1 and menu is called by:"+arguments.callee.name);
 }
+function menu3(){
+	alert("menu number is 3 and menu is called by:"+arguments.callee.name);
+}
+function menu4(){
+	alert("menu number is 4 and menu is called by:"+arguments.callee.name);
+}
+function menu7(){
+	alert("menu number is 7 and menu is called by:"+arguments.callee.name);
+}
+function menu9(){
+	alert("menu number is 9 and menu is called by:"+arguments.callee.name);
+}
+function menu10(){
+	alert("menu number is 10 and menu is called by:"+arguments.callee.name);
+}
 function menu12(){
 	alert("menu number is 12 and menu is called by:"+arguments.callee.name);
 }
