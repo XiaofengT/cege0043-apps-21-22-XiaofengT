4863ff376bb2b935f758fe6d9a13804dc899df34
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-02-05 15:40:51 +0000

    Adding menu 11 and menu 14

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,15 @@
 function menu1(){
 	alert("menu number is 1 and menu is called by:"+arguments.callee.name);
 }
+function menu12(){
+	alert("menu number is 12 and menu is called by:"+arguments.callee.name);
+}
+function menu13(){
+	alert("menu number is 13 and menu is called by:"+arguments.callee.name);
+}
+function menu15(){
+	alert("menu number is 15 and menu is called by:"+arguments.callee.name);
+}
+function menu16(){
+	alert("menu number is 16 and menu is called by:"+arguments.callee.name);
+}
