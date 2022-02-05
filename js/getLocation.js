e986da835a70fb7f23c2e235177a0263ce253c3d
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-02-05 20:02:46 +0000

    Adding getLocation function

---
+++ js/getLocation.js
@@ -0,0 +1,7 @@
+function getLocation(){
+	alert('getting location');
+	navigator.geolocation.getCurrentPosition(getPosition);
+}
+function getPosition(position){
+	document.getElementById('showLocation').innerHTML="Latitude:"+position.coords.latitude+"<br>Longitude:"+position.coords.longitude;
+}
