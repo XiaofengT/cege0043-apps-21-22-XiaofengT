840e2787954f4bbd946cd4fea73372fc4165f3f8
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-03-08 23:56:07 +0000

    Removed useless file and code

--- js/centennial.js
+++ js/centennial.js
@@ -1,3 +1,24 @@
+// the following script will load the map and set the default view and zoom, as well as loading the basemap tiles
+// load the map
+var mymap;
+// create an array to store all the location tracking points
+var trackLocationLayer =[];
+// store the ID of the locaiton tracker so that it can be used to switch the location tracking off 
+var geoLocationID;
+// load the tiles
+function loadLeafletMap(){
+mymap = L.map('mapContainer').setView([51.505, -0.09], 9);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+maxZoom: 18,
+attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+id: 'mapbox/streets-v11',
+tileSize: 512,
+zoomOffset: -1
+}).addTo(mymap);
+}
+
 // Buildings data
 var buildingLayer;
 
