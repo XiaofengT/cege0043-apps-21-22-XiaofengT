14ca8a8c3f07cbbaaa391fcb38afef60bca01d3f
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-03-08 20:21:27 +0000

    Added function to show and close the graph

---
+++ js/graph.js
@@ -0,0 +1,7 @@
+function closeGraph() {
+	document.getElementById("graphWrapper").style.top = "-9999px";
+}
+function showGraph() {
+document.getElementById("graphWrapper").style.top = "300px";
+// the code to generate the graph goes here
+}
