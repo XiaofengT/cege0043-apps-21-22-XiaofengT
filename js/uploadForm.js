568fe7a2eeda7ab8dd2d0f546a9dba67e8635ffa
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-03-21 20:37:01 +0000

    Added uploadForm file

---
+++ js/uploadForm.js
@@ -0,0 +1,36 @@
+function startDataUpload() {
+	alert ("start data upload");
+
+	var asset_name = document.getElementById("asset_name").value;
+	var installation_date = document.getElementById("installation_date").value;
+	var postString = "asset_name="+asset_name +"&installation_date="+installation_date;
+	
+	// now get the geometry values
+	var latitude = document.getElementById("latitude").value;
+	var longitude = document.getElementById("longitude").value;
+	postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
+	
+	processData(postString);
+
+}
+
+
+
+function processData(postString) {
+	alert(postString);
+
+	var serviceUrl=  document.location.origin + "/api/testCRUD";
+   $.ajax({
+    url: serviceUrl,
+    crossDomain: true,
+    type: "POST",
+    data: postString,
+    success: function(data){console.log(data); dataUploaded(data);}
+
+}); 
+}
+// create the code to process the response from the data server
+function dataUploaded(data) {
+    // change the DIV to show the response
+    document.getElementById("dataUploadResult").innerHTML = JSON.stringify(data);
+}
