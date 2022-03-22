5186aa3c02f885a64e554196b363e8492b9ddc85
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-03-22 14:26:16 +0000

    Initialized bootstrap menu list

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+function menu1(){
+	alert("this function shows list of assets in best condition "+arguments.callee.name);
+}
+function menu2(){
+	alert("this function shows daily reporting rates graph "+arguments.callee.name);
+}
+function menu3(){
+	alert("this function shows help "+arguments.callee.name);
+}
+function menu4(){
+	alert("this function shows user ranking "+arguments.callee.name);
+}
+function menu5(){
+	alert("this function add layer - 5 closest assets "+arguments.callee.name);
+}
+function menu6(){
+	alert("this function remove layer - 5 closest assets "+arguments.callee.name);
+}
+function menu7(){
+	alert("this function add layer - last 5 reports, colour coded "+arguments.callee.name);
+}
+function menu8(){
+	alert("this function remove layer - last 5 reports "+arguments.callee.name);
+}
+function menu9(){
+	alert("this function add layer - not rated in the last 3 days "+arguments.callee.name);
+}
+function menu10(){
+	alert("this function remove layer - not rated in the last 3 days "+arguments.callee.name);
+}
