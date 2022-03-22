90d433f4bec5b7ce99fbda937a56959fe8e240f0
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-03-22 15:46:46 +0000

    Added setMapClickEvent function

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,46 @@
+// the following script will load the map and set the default view and zoom, as well as loading the basemap tiles
+// load the map
+var mymap;
+// load the tiles
+function loadLeafletMap(){
+mymap = L.map('mapContainer').setView([51.505, -0.09], 9);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+maxZoom: 18,
+attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+id: 'mapbox/streets-v11',
+tileSize: 512,
+zoomOffset: -1
+}).addTo(mymap);
+}
+
+var width; // NB – keep this as a global variable
+var popup; // keep this as a global variable
+var mapPoint; // store the geoJSON feature so that we can remove it if the screen is resized
+function setMapClickEvent() {
+	// get the window width
+	width = $(window).width();
+	// we use the bootstrap Medium and Large options for the asset location capture
+	// and the small and XS options for the condition option
+	// see here: https://www.w3schools.com/bootstrap/bootstrap_grid_system.asp
+	if (width < 992) {//the condition capture – 992px is defined as 'medium' by bootstrap
+		// cancel the map onclick event using off ..
+		mymap.off('click',onMapClick)
+		// set up a point with click functionality
+		setUpPointClick(); 
+	}
+	else { // the asset creation page
+		// remove the map point if it exists
+		if (mapPoint){
+			mymap.removeLayer(mapPoint);
+		}
+		// the on click functionality of the MAP should pop up a blank asset creation form
+		mymap.on('click', onMapClick);
+	}
+}
+
+
 function menu1(){
 	alert("this function shows list of assets in best condition "+arguments.callee.name);
 }
