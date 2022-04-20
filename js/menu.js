d1b0beb632d6b8a1272f5ecf09e1cde6eceb1989
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-04-20 16:48:44 +0100

    Added function of loading daily reporting rates graph

--- js/menu.js
+++ js/menu.js
@@ -192,13 +192,50 @@ function removeMissingCondition() {
 	}
 }
 
-function getDailyReportsRates() {
-	var reportJSON;
+function showReportGraph() {
+	document.getElementById("graphWrapper").style.top="15%";
+	var widtha = document.getElementById("graphWrapper").offsetWidth;
+	var heighta = document.getElementById("graphWrapper").offsetHeight;
+	// keep the existing HTML as there is a button that is needed
+	document.getElementById("graphWrapper").innerHTML=document.getElementById("graphWrapper").innerHTML+'<div class="h-75 w-75"><svg width="'+widtha+'" height="'+heighta+'" id="svg1"></svg></div>'
+	const svg = d3.select("#svg1"),
+	margin = {top: 20, right: 20, bottom: 30, left: 50},
+	width = +svg.attr("width") - margin.left - margin.right,
+	height = +svg.attr("height") - margin.top - margin.bottom,
+	x = d3.scaleBand().rangeRound([0, width]).padding(0.2),
+	y = d3.scaleLinear().rangeRound([height, 0]),
+	g = svg.append("g")
+		.attr("transform", `translate(${margin.left},${margin.top})`); 
 	var baseComputerAddress = document.location.origin;
 	var getReportString = "/api/dailyParticipationRates";
 	var getReportURL = baseComputerAddress + getReportString;
-	$.ajax({url: getReportURL, crossDomain: true, async: false, success: function(result){
-		 reportJSON = result;
+	$.ajax({url: getReportURL, crossDomain: true, success: function(result){
+		data = result[0]["array_to_json"];
+		x.domain(data.map(data => data.day));
+		y.domain([0, d3.max(data, data => data.reports_submitted)]);
+		
+		g.append("g")
+			.attr("class", "axis axis-x")
+			.attr("transform", `translate(0,${height})`)
+			.call(d3.axisBottom(x));
+		g.append("g")
+			.attr("class", "axis axis-y")
+			.call(d3.axisLeft(y).ticks(10).tickSize(8)); 
+		g.selectAll(".bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", d => x(d.day))
+			.attr("y", d => y(d.reports_submitted))
+			.attr("width", x.bandwidth())
+			.attr("height", d => height - y(d.reports_submitted));
+		// .catch(err => {
+		// svg.append("text")         
+			// .attr("y", 20)
+			// .attr("text-anchor", "left")  
+			// .style("font-size", "20px") 
+			// .style("font-weight", "bold")  
+			// .text(`Couldn't open the data file: "${err}".`);
+		// });
 	}});
-	return reportJSON;
 }
