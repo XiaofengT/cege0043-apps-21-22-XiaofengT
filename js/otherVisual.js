6b427f498135e7e882e730f3506e805abd87387e
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-04-23 23:54:07 +0100

    Adjusted the table content layout

--- js/otherVisual.js
+++ js/otherVisual.js
@@ -1,11 +1,11 @@
 function rankTable(){
 	var getTableURL = document.location.origin + "/api/topFiveScorers/";
 	$.ajax({url: getTableURL, dataType: "json", success: function(result){
-		var tableHTML = '<table><tbody>';
+		var tableHTML = '<table class="table mb-0" style="width: 100%;height: 100%;"><tbody>';
 		var rankingData = result[0]["array_to_json"];
 		for (var i=0; i<rankingData.length; i++){
-			tableHTML += "<tr>"+"<td>No. "+rankingData[i].rank+"</td>";
-			tableHTML += "<td>User "+rankingData[i].user_id+"</td>";
+			tableHTML += '<tr style="white-space: nowrap;">'+"<td>No. "+rankingData[i].rank+"</td>";
+			tableHTML += '<td style="white-space: nowrap;">User '+rankingData[i].user_id+"</td>";
 			tableHTML += "</tr>";
 		}
 		tableHTML += "<tbody></table>";
