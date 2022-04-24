c9c3b07c8d82540ee8c61ea02e94e6b558f380e7
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-04-24 01:34:38 +0100

    Added the condition percentage pie chart to the dashboard

--- js/otherVisual.js
+++ js/otherVisual.js
@@ -12,3 +12,65 @@ function rankTable(){
 		document.getElementById("rankingTable").innerHTML = tableHTML;
 	}})
 }
+
+function showPieChart(){
+	var baseComputerAddress = document.location.origin;
+	var getAssetsString = "/api/geoJSONUserId/" + user_id;
+	var getReportURL = baseComputerAddress + getAssetsString;
+	var description = ["Element is in very good condition", 
+                  "Some aesthetic defects, needs minor repair", 
+                  "Functional degradation of some parts, needs maintenance", 
+                  "Not working and maintenance must be done as soon as reasonably possible", 
+                  "Not working and needs immediate, urgent maintenance", 
+                  "Unknown"];
+	var barColor = ["blue", "green", "yellow", "orage", "red", "gray"];
+	var condition_count = [0,0,0,0,0,0];
+	$.ajax({url: getReportURL, crossDomain: true, async: false, success: function(result){
+		data = result[0]["features"];
+		for (var i=0; i<data.length; i++){
+			var condition_name = data[i]["properties"]["condition_description"];
+			if(condition_name==description[0]){
+				condition_count[0] += 1;
+			}
+			else if(condition_name==description[1]){
+				condition_count[1] += 1;
+			}
+			else if(condition_name==description[2]){
+				condition_count[2] += 1;
+			}
+			else if(condition_name==description[3]){
+				condition_count[3] += 1;
+			}
+			else if(condition_name==description[4]){
+				condition_count[4] += 1;
+			}
+			else if(condition_name==description[5]){
+				condition_count[5] += 1;
+			}
+			else{
+				condition_count[6] += 1;
+			}
+		}
+		console.log(condition_count);
+		console.log(description);
+		new Chart(document.getElementById("percentageGraph"), {
+			type: "doughnut",
+			data: {
+			  labels: description,
+			  datasets: [{
+				data: condition_count,
+				backgroundColor: barColor,
+				borderWidth: 1,
+			  }]
+			},
+			options: {
+			  maintainAspectRatio: false,
+			  responsive: true,
+			//  cutoutPercentage: 
+			  legend: {
+				display: true,
+			  }
+			}
+      });
+	}})
+}
